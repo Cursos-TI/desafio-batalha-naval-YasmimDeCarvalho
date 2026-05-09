77fa06cb67eeac49d8fd1d6b5a6bf17a3de8b34c
YasmimDeCarvalho <yayaafonso1996@gmail.com>
2026-05-08 19:05:09 -0700

    batalha naval finalizada

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,86 +1,131 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#define TAB 10
 #define LINHA 10
 #define COLUNA 9
+#define HAB 5
+
+void cone(int array[TAB][TAB]);
+void cruz(int array[TAB][TAB]);
+void octaedro(int array[TAB][TAB]);
+void finalWar(int array[TAB][TAB]);
 
 int main() {
 
-    int navyWar[10][10] = {{0,0,0,0,0,0,0,0,0,0},
-                           {0,0,0,0,0,0,0,0,0,0},
-                           {0,0,0,0,0,0,0,0,0,0},
-                           {0,0,0,0,0,0,0,0,0,0},
-                           {0,0,0,0,0,0,0,0,0,0},
-                           {0,0,0,0,0,0,0,0,0,0},
-                           {0,0,0,0,0,0,0,0,0,0},
-                           {0,0,0,0,0,0,0,0,0,0},
-                           {0,0,0,0,0,0,0,0,0,0},
-                           {0,0,0,0,0,0,0,0,0,0}};
+    int navyWar[TAB][TAB] = {{0}};
+
+//----------navios------------//
+    navyWar[2][3] = 3;
+    navyWar[2][4] = 3;
+    navyWar[2][5] = 3;
+
+//----------shapes----------//
+    cone(navyWar);
+    cruz(navyWar);
+    octaedro(navyWar);
+
+    finalWar(navyWar);
+
+
 
     
-    printf("Tabuleiro inicial:\n");
-    for(int x = 0 ; x < LINHA ; x++){
-        for(int y = 0 ; y <= COLUNA ; y++){
-            printf("%d ", navyWar[x][y]);
-        }
-        printf("\n");
-    }
 
-    printf("\nAdicionando navios:\n");
-    // Navio horizontal
-    for (int c = 1; c <= 3; c++) {
-        navyWar[3][c] = 3;
-    }
-
-    // Navio vertical
-    for (int l = 4; l <= 6; l++) {
-        navyWar[l][7] = 3;
-    }
-
-    // Navio diagonal principal (↘)
-    for (int i = 0; i < 4; i++) {
-        navyWar[i][i] = 3;
-    }
-
-    // Navio diagonal secundária
-    for (int i = 0; i < 4; i++) {
-        navyWar[i][9 - i] = 3;
-    }
-
-        for (int l = 0; l < LINHA; l++) {
-            for (int c = 0; c < COLUNA; c++) {
-                 printf("%d ", navyWar[l][c]);
-                }
-        printf("\n");
-    }
-
-
-
-
+    
    
 }
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+void cone(int array[TAB][TAB]){
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    int origemLinha = 4;
+    int origemColuna = 4;
+
+    for (int i = 0; i < HAB; i++) {
+        for (int j = 0; j < HAB; j++) {
+
+            if (j >= HAB/2 - i && j <= HAB/2 + i) {
+
+                int l = origemLinha + (i - HAB/2);
+                int c = origemColuna + (j - HAB/2);
+
+                if (l >= 0 && l < TAB && c >= 0 && c < TAB) {
+                    if (array[l][c] != 3) array[l][c] = 5;
+                }
+            }
+        }
+    }
+
+ 
+    // for(int x = 0 ; x < LINHA ; x++){
+    //     for(int y = 0 ; y <= COLUNA ; y++){
+    //         printf("%d ", array[x][y]);
+    //     }
+    //     printf("\n");
+        
+    // }
+
+
+}
+
+void cruz(int array[TAB][TAB]) {
+    int origemLinha = 4;   // centro do tabuleiro
+    int origemColuna = 4;
+
+    for (int i = 0; i < HAB; i++) {
+        for (int j = 0; j < HAB; j++) {
+
+            if (i == HAB/2 || j == HAB/2) {   // forma da cruz
+
+                int l = origemLinha + (i - HAB/2);
+                int c = origemColuna + (j - HAB/2);
+
+               if (array[l][c] != 3) array[l][c] = 5;
+            }
+        }
+    }
+
+    // for(int x = 0 ; x < TAB ; x++){
+    //     for(int y = 0 ; y < TAB ; y++){
+    //         printf("%d ", array[x][y]);
+    //     }
+    //     printf("\n");
+    // }
+}
+
+void octaedro(int array[TAB][TAB]) {
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    int origemLinha = 4;
+    int origemColuna = 4;
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    for (int i = 0; i < HAB; i++) {
+        for (int j = 0; j < HAB; j++) {
+
+            if (abs(i - HAB/2) + abs(j - HAB/2) <= HAB/2) {
+
+                int l = origemLinha + (i - HAB/2);
+                int c = origemColuna + (j - HAB/2);
+
+                if (array[l][c] != 3) array[l][c] = 5;
+            }
+        }
+    }
+
+    
+    // for(int x = 0 ; x < TAB ; x++){
+    //     for(int y = 0 ; y < TAB ; y++){
+    //         printf("%d ", array[x][y]);
+    //     }
+    //     printf("\n");
+    // }
+}
+
+void finalWar(int array[TAB][TAB]) {
+    for (int x = 0; x < TAB; x++) {
+        for (int y = 0; y < TAB; y++) {
+            printf("%d ", array[x][y]);
+        }
+        printf("\n");
+    }
+}
+
